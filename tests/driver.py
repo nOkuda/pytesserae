80067fadb545e64a944c7802800c24396a573ac0
Eliabeth Soper <esoper@buffalo.edu>
2017-10-25 13:34:20 -0400

    Driver now gives option to choose output format.

--- tests/driver.py
+++ tests/driver.py
@@ -1,5 +1,6 @@
 import parse_highlight
 import html_output
+import latex_output
 import argparse
 
 parser = argparse.ArgumentParser(description='Search file for regex')
@@ -10,8 +11,18 @@ fileName = args.inputFile
 search = args.search
 
 matchList = parse_highlight.searchify(fileName, search)
-output = html_output.html(matchList)
+keypress = input('Specify output file type: \'h\' for html or \'l\' for LaTeX\n')
+if keypress == 'h':
+    output = html_output.html(matchList)
+    with open('SearchResult.html', 'w') as f:
+        for l in output:
+            f.write(l + '</br>')
 
-with open('SearchResult.html', 'w') as f:
-    for l in output:
-        f.write(l + '</br>')
+elif keypress == 'l':
+    output = latex_output.latex(matchList)
+    with open('SearchResult.tex', 'w') as f:
+        for l in output:
+            f.write(l)
+
+else:
+    print('Error!')
