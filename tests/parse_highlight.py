7cd08ef7fa8491c71ce59dc015087c9dc594b268
Eliabeth Soper <esoper@buffalo.edu>
2017-10-25 10:27:32 -0400

    fixes arguments on searchify

--- tests/parse_highlight.py
+++ tests/parse_highlight.py
@@ -1,14 +1,6 @@
 import argparse
 import re
 
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Search file for reg expression.')
-	parser.add_argument("inputFile", help="name of file to search")
-	parser.add_argument("search", help="expression to search for")
-	args = parser.parse_args()
-	filePath = args.inputFile
-	searchTerm = args.search
-
 def searchify(filePath, searchTerm):
     with open(filePath, 'r') as f:
         foundLines = []
@@ -19,3 +11,13 @@ def searchify(filePath, searchTerm):
                 highlight = line.replace(literal, '*{}*'.format(literal))
                 foundLines.append(highlight)
                 return(foundLines)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Search file for regex')
+    parser.add_argument("inputFile", help="name of file to search")
+    parser.add_argument("search", help="expression to search for")
+    args = parser.parse_args()
+    file = args.inputFile
+    search = args.search
+        
+    searchify(file, search)
