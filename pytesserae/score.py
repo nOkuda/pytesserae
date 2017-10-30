409116cddfb14a2d74b78affdedfa2abb5a2ae85
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2017-10-30 12:19:42 -0400

    Implemented Tesserae scoring function

--- pytesserae/score.py
+++ pytesserae/score.py
@@ -1,14 +1,38 @@
 """Module for Tesserae scoring"""
+import math
 
 
-def vanilla(hoc, illud, counts):
-    """Calculates the Tesserae score between hoc and illud
+def vanilla(
+    matching_terms,
+    source_distance, target_distance,
+    source_counts, target_counts
+):
+    """Calculates the Tesserae score between from_source and from_target
 
-        * hoc, illud :: [str]
-            A list of words to compare with each other
-        * counts :: {str: int}
-            Tells how many times a given word occurs in the current definition
-            of corpus.
+        * matching_terms :: {str}
+            A set of words found to match between the source and target
+        * source_distance, target_distance :: int
+            Distance between least frequent matching terms for source and
+            target, respectively
+        * source_counts, target_counts :: {str: int}
+            A dictionary of word counts to consult in looking up frequency
+            information
+
+    score = ln (
+        (
+            sum([1/f(t) for t in matching_terms]) +
+            sum([1/f(s) for s in matching_terms])
+        )
+        / (d_t + d_s)
+    )
+        * f(t) is the frequency of a matching term in the target
+        * f(s) is the frequency of a matching term in the source
+        * d_t = target_distance
+        * d_s = source_distance
     """
-    # TODO:  Actually implement
-    return 0.0
+    return math.log(
+        (
+            sum([1 / target_counts[t] for t in matching_terms]) +
+            sum([1 / source_counts[s] for s in matching_terms])
+        ) / (target_distance + source_distance)
+    )
