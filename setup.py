6d9048064be636743b0180eb01d93a5d9c6d46d2
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2018-03-09 13:41:21 -0500

    Make pytesserae pip installable

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytesserae',
+    version='0.0.1',
+    description='Tesserae v5 sandbox',
+    url='https://github.com/nOkuda/pytesserae',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 1 - Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ]
+    keywords='Tesserae',
+    python_requires='~=3.5',
+    packages=find_packages(exclude=['examples', 'tests']),
+)
